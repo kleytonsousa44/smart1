e634dfd66142216cd557c32d684f2342d27c6a84
kleytonsousa44 <130249895+kleytonsousa44@users.noreply.github.com>
2023-06-23 10:25:17 -0300

    origin

---
+++ frontend/build/precache-manifest.ee339d9f63e74594b68f1ba1c61840a4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64a4e5661fd81464b59c58028e10950",
+    "url": "/index.html"
+  },
+  {
+    "revision": "663186ed5b550d155eb3",
+    "url": "/static/css/2.38dcfae8.chunk.css"
+  },
+  {
+    "revision": "663186ed5b550d155eb3",
+    "url": "/static/js/2.6af8a8ec.chunk.js"
+  },
+  {
+    "revision": "334503b5ea74617d0ea6c9f43e18112f",
+    "url": "/static/js/2.6af8a8ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811da90634f4310ee7f5",
+    "url": "/static/js/3.23d7d834.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/static/js/3.23d7d834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e33a53d3ecbb4c89b367",
+    "url": "/static/js/main.6974440e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.6974440e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8951132bb5d4350a8bd1",
+    "url": "/static/js/runtime-main.fa0d4038.js"
+  },
+  {
+    "revision": "e5731dac0af3f3f76647f7ef913fa5b0",
+    "url": "/static/media/logo.e5731dac.png"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/static/media/wa-background.adbccee0.png"
+  }
+]);
